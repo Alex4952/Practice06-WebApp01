906a04807945ba885fc03b3387f8de74dcf639d0
Mac <Mac@macui-imac-6.local>
2015-10-31 12:49:12 +0900

    웹뷰로 네이버 화면 띄우기 (ATS 보안설정 필요)

--- Practice06-WebApp01/ViewController.swift
+++ Practice06-WebApp01/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+	@IBOutlet var wv: UIWebView!
+	
+	// 화면이 처음 만들어질 때 딱 한번만 호출됨.
 	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		//		super.viewDidLoad()
+		
+		let url = NSURL(string: "http://www.naver.com")
+		let req = NSURLRequest(URL: url!)
+		
+		wv.loadRequest(req)
+		
+		// ATS (App Transport Security)
+		// ios9 부터 https 만 허용하도록 규정이 바뀜
+		// info.plist 우클릭 -> Open As -> Source Code
+//		<key>NSAppTransportSecurity</key>
+//		<dict>
+//		<key>NSAllowsArbitraryLoads</key>
+//		<true/>
+//		</dict>
+
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
+	
 }
 
